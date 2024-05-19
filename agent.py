2d86a10e32d7ddf18b99eae01babc3bf5c51b895
Sarthak Rastogi <61943500+sarthakrastogi@users.noreply.github.com>
2024-05-19 16:23:34 +0530

    add react

--- agent.py
+++ agent.py
@@ -36,10 +36,8 @@ class PlannerAgent(Agent):
 
     def act(self, instructions=[]):
         if self.plan_steps and not self.planner:
-            print("GOT", self.plan_steps, self.task)
             self.planner = Planner(agent_goal=self.task)
             self.planner.from_preplanned_steps(self.plan_steps)
-            print("CREATED PLANNER", self.planner.agent_plan_steps, self.planner.agent_goal)
 
         elif not self.plan_steps and self.planner:
             self.planner.plan_task(self.abilities, brain=self.brain)
@@ -62,3 +60,57 @@ class PlannerAgent(Agent):
             print(f"""Performed {instruction_function.__name__} with args {str(instruction_arguments)}.\nResult: {result}""")
             prev_step = f"""The previous step was:\n{plan_step}\nThe previous step's output was:\n{result}"""
         return result
+    
+
+
+class ReActAgent(Agent):
+    def __init__(self, brain, task, long_term_memory, name="", purpose="", instructions=[]):
+        super().__init__(brain, task, long_term_memory, name, purpose, instructions)
+
+    def create_ReAct_thought(previous_iteration):
+        create_ReAct_thought_system_prompt = f"""
+        You are part of an AI agent.
+        This is the process the agent has completed so far:
+        {previous_iteration}
+        """
+
+    def create_ReAct_action(ReAct_thought):
+        pass
+
+    def create_ReAct_observation(ReAct_thought, ReAct_action):
+        pass
+
+    def act(self):
+        previous_iteration = ""
+        while True:
+            ReAct_thought = self.create_ReAct_thought(previous_iteration)
+            ReAct_action = self.create_ReAct_action(ReAct_thought=ReAct_thought)
+            ReAct_observation = self.create_ReAct_observation(ReAct_thought=ReAct_thought, ReAct_action=ReAct_action)
+            
+
+        if self.plan_steps and not self.planner:
+            self.planner = Planner(agent_goal=self.task)
+            self.planner.from_preplanned_steps(self.plan_steps)
+ 
+        elif not self.plan_steps and self.planner:
+            self.planner.plan_task(self.abilities, brain=self.brain)
+            self.plan_steps = self.planner.agent_plan_steps
+
+        elif not self.plan_steps and not self.planner:
+            raise Exception("Provide either a Planner class object or plan_steps to the Agent class.")
+        return self.execute_plan()
+            
+    def execute_plan(self):
+        for step in self.plan_steps:
+            print(step)
+        prev_step, result = "", ""
+        for plan_step in self.plan_steps:
+            plan_step = prev_step + plan_step
+            instructions_llm_response = self.planner.convert_plan_step_to_instruction(plan_step, self.abilities)
+            instruction_function = [ability.action for ability in self.abilities if ability.ability_name == instructions_llm_response['ability_name']][0]
+            instruction_arguments = instructions_llm_response['arguments']
+            result = instruction_function(**instruction_arguments)
+            print(f"""Performed {instruction_function.__name__} with args {str(instruction_arguments)}.\nResult: {result}""")
+            prev_step = f"""The previous step was:\n{plan_step}\nThe previous step's output was:\n{result}"""
+        return result
+    
